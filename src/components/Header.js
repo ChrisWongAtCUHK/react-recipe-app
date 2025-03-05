d1f873b5f8c221a3b55cabc8983bbc66b53451a6
Chris Wong <chris.wong.ksl@gmail.com>
2025-03-05 19:42:29 +0800

    Active link color

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,27 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouse, faHeart } from '@fortawesome/free-solid-svg-icons'
 
 import './Header.css'
 
 function Header() {
+  const location = useLocation()
+
   return (
     <header className='main_header' id='main-header'>
       <nav className='headerMain' id='mobile-nav'>
         <ul data-id='menuList'>
           <li data-id='menuList' className='menuLink'>
-            <Link to="/"><FontAwesomeIcon icon={faHouse}/>Home</Link>
+            <Link to='/' className={location.pathname === '/' ? 'activeLink' : ''}>
+              <FontAwesomeIcon icon={faHouse} />
+              Home
+            </Link>
           </li>
           <li data-id='menuList' className='menuLink'>
-            <Link to="/favorites"><FontAwesomeIcon icon={faHeart}/>Favorites</Link>
+            <Link to='/favorites' className={location.pathname === '/favorites' ? 'activeLink' : ''}>
+              <FontAwesomeIcon icon={faHeart} />
+              Favorites
+            </Link>
           </li>
         </ul>
       </nav>
