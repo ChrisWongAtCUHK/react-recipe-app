de77247d9c390e293dc1272f31a47c49ad62b1c1
Chris Wong <chris.wong.ksl@gmail.com>
2025-03-06 14:23:55 +0800

    In progress: saveFavRecipe

--- src/components/FoodCard.js
+++ src/components/FoodCard.js
@@ -1,8 +1,42 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  selectRecipe,
+  loadFavMeals,
+  setFavMeals,
+} from '../features/slices/recipeSlice'
 import './FoodCard.css'
 
 function FoodCard({ meal }) {
+  const favMeals = useSelector(selectRecipe).favMeals
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(loadFavMeals())
+  }, [dispatch])
+
+  function saveFavRecipe() {
+    if (isFav()) {
+      // set off favorite
+      const isFavoriteYet = favMeals.filter((fav) => fav.idMeal !== meal.idMeal)
+      dispatch(setFavMeals({ favMeals: isFavoriteYet }))
+
+      return
+    }
+
+    const newFavRecipes = [{ ...meal }, ...favMeals]
+    dispatch(setFavMeals({ favMeals: newFavRecipes }))
+  }
+
+  function isFav() {
+    console.log(favMeals)
+    return favMeals
+      ? favMeals.find((recipe) => recipe.idMeal === meal.idMeal)
+      : false
+  }
+
   return (
     <div className='containerFood'>
       <div className='card-header'>
@@ -11,7 +45,7 @@ function FoodCard({ meal }) {
             ? `${meal.strMeal.substring(0, 20)} ...`
             : meal.strMeal}
         </h3>
-        <button className='favButton'>
+        <button className='favButton' onClick={saveFavRecipe}>
           <FontAwesomeIcon icon={faHeart} />
         </button>
       </div>
--- src/features/slices/recipeSlice.js
+++ src/features/slices/recipeSlice.js
@@ -1,7 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+const RECIPES_APP = '@RecipesApp'
+
 const initialState = {
-  isLoading: false
+  isLoading: false,
+  notificationsInfos: {},
+  favMeals: [],
 }
 
 const recipeSlice = createSlice({
@@ -11,10 +15,23 @@ const recipeSlice = createSlice({
     setIsLoading: (state, actions) => {
       state.isLoading = actions.payload.isLoading
       return state
-    }
-  }
+    },
+    setNotificationsInfos: (state, actions) => {
+      state.notificationsInfos = { ...actions.payload.infos }
+      return state
+    },
+    loadFavMeals: (state) => {
+      state.favMeals = JSON.parse(localStorage.getItem(RECIPES_APP)) || []
+      return state
+    },
+    setFavMeals: (state, actions) => {
+      state.favMeals = [...actions.payload.favMeals]
+      localStorage.setItem(RECIPES_APP, JSON.stringify(state.favMeals)) 
+      return state
+    },
+  },
 })
 
 export const selectRecipe = (state) => state.recipe
-export const { setIsLoading } = recipeSlice.actions
+export const { setIsLoading, setNotificationsInfos, loadFavMeals, setFavMeals } = recipeSlice.actions
 export default recipeSlice.reducer
